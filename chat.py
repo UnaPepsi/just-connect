33a7d8d107a095ab9ff6ceb6f9123fdfca0de2ab
UnaPepsi <unapepsixd@gmail.com>
2024-08-06 19:42:34 -0600

    Initial commit

---
+++ chat.py
@@ -0,0 +1,15 @@
+import customtkinter as ctk
+
+window = ctk.CTk()
+
+text = ctk.CTkTextbox(window)
+
+scroll = ctk.CTkScrollbar(window)
+scroll.pack(side='right', fill='y')
+
+scroll.configure(command=text.yview)
+text.configure(yscrollcommand=scroll.set)
+text.pack(side='left', fill='both', expand=True)
+
+
+window.mainloop()
